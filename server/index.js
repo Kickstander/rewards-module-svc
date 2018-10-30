af4cb061c4137a25ac3dbffd1f55828eee6df97e
David Lee <edenlhn@gmail.com>
2018-10-29 19:16:52 -0700

    Set up basic route for API get rqst

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../database/index.js');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.get('/api/:projectId/rewards', (req, res) => {
+  let projectId = req.params.projectId;
+
+  db.Reward.findAll({
+    where: {
+      projectId: projectId
+    },
+    order: [
+      ['pledgeAmount', 'ASC']
+    ]
+  })
+    .then((rewards) => {
+      rewards = rewards.map((reward) => {
+        return reward.dataValues;
+      });
+      res.send(rewards);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
