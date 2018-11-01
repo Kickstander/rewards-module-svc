346e9745ac1c96ec4a1f82f63f831fbc9885720f
David Lee <edenlhn@gmail.com>
2018-11-01 11:11:23 -0700

    Added styled components and event handlers to widget

--- client/components/RewardTier.jsx
+++ client/components/RewardTier.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
-
-const Wrapper = styled.div`
-  position: relative;
-  width: 20%;
-  border: solid 1px;
-  margin-bottom: 20px;
-`;
-
-const Overlay = styled.div`
-background-color: rgba(0, 158, 116, 0.0);
-color: rgba(255, 255, 255, 0.0);
-position: absolute;
-top: 0;
-width: 100%;
-height: 100%;
-
-:hover {
-  background-color: rgba(0, 158, 116, 0.75);
-  color: rgba(255, 255, 255, 1.0);
-}
-`;
+import MiniPledgeForm from './MiniPledgeForm.jsx';
 
 class RewardTier extends React.Component {
   constructor(props) {
@@ -31,36 +10,125 @@ class RewardTier extends React.Component {
     };
 
     this.handleWidgetClick = this.handleWidgetClick.bind(this);
+    this.renderView = this.renderView.bind(this);
   }
 
-  handleWidgetClick(e) {
-    //some shit
-    console.log(e.target);
+  handleWidgetClick() {
+    this.setState({
+      view: 'clicked'
+    });
+  }
+
+  renderView() {
+    let {view} = this.state;
+    if (view === 'clicked') {
+      return (
+        <StyledMiniPledgeForm>
+          <MiniPledgeForm reward={this.props.reward}/>
+        </StyledMiniPledgeForm>
+      );
+    }
   }
 
   render() {
     return (
-      <Wrapper>
-        <div id={`${this.props.reward.id}`} className='rewardTier' onClick={this.handleWidgetClick}>
-          <div className='pledgeAmount'>Pledge US$ {this.props.reward.pledgeAmount} or more</div>
-          <div className='rewardName'>{this.props.reward.name}</div>
-          <div className='rewardDesc'>{this.props.reward.description}</div>
+      <RewardWrapper>
+        <TierWrapper id={`${this.props.reward.id}`} className='rewardTier'>
+          <TitleWrapper className='pledgeAmount'>Pledge US$ {this.props.reward.pledgeAmount} or more</TitleWrapper>
+          <RewardName className='rewardName'>{this.props.reward.name}</RewardName>
+          <RewardDesc className='rewardDesc'>{this.props.reward.description}</RewardDesc>
           <div className='rewardItems'>
-            INCLUDES:
+            <GenericWrapper>INCLUDES:</GenericWrapper>
             <ul>
-              <li>{this.props.reward.item1}</li>
-              <li>{this.props.reward.item2}</li>
-              <li>{this.props.reward.item3}</li>
+              <ListWrapper>{this.props.reward.item1}</ListWrapper>
+              <ListWrapper>{this.props.reward.item2}</ListWrapper>
+              <ListWrapper>{this.props.reward.item3}</ListWrapper>
             </ul>
           </div>
-          <div className='estDeliv'>ESTIMATED DELIVERY {this.props.reward.estDeliv}</div>
-          <div className='shipsTo'>SHIPS TO {this.props.reward.shipsTo}</div>
-          <div className='backers'>{this.props.reward.backers} backers</div>
-        </div>
-        <Overlay>Select this reward</Overlay>
-      </Wrapper>
+          <div className='estDeliv'>
+            <GenericWrapper>ESTIMATED DELIVERY</GenericWrapper>
+            <ContentWrapper>{this.props.reward.estDeliv}</ContentWrapper>
+          </div>
+          <div className='shipsTo'>
+            <GenericWrapper>SHIPS TO</GenericWrapper>
+            <ContentWrapper>{this.props.reward.shipsTo}</ContentWrapper>
+          </div>
+          <GenericWrapper className='backers'>{this.props.reward.backers} backers</GenericWrapper>
+        </TierWrapper>
+        <Overlay onClick={this.handleWidgetClick}>Select this reward</Overlay>
+        {this.renderView()}
+      </RewardWrapper>
     );
   }
 }
+
+// styled components for RewardTier component
+const RewardWrapper = styled.div`
+  position: relative;
+  width: 18%;
+  border: solid 1px;
+  margin-bottom: 20px;
+`;
+
+const Overlay = styled.div`
+  background-color: rgba(0, 158, 116, 0.0);
+  color: rgba(255, 255, 255, 0.0);
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+  font-family: 'Barlow', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover {
+    background-color: rgba(0, 158, 116, 0.75);
+    color: rgba(255, 255, 255, 1.0);
+    cursor: pointer;
+  }
+`;
+
+const StyledMiniPledgeForm = styled.div`
+  margin: 15px;
+`;
+
+const TierWrapper = styled.div`
+  margin: 15px;
+`;
+
+const TitleWrapper = styled.div`
+  font-family: 'Barlow', sans-serif;
+  font-size: 22px;
+`;
+
+const RewardName = styled.div`
+  font-family: 'Barlow', sans-serif;
+  font-size: 20px;
+  margin-top: 15px;
+`;
+
+const RewardDesc = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+  margin-top: 10px;
+`;
+
+const ListWrapper = styled.li`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+`;
+
+const GenericWrapper = styled.div`
+  font-family: 'Raleway', sans-serif;
+  font-size: 10px;
+  margin-top: 10px;
+`;
+
+const ContentWrapper = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+`;
 
 export default RewardTier;
